fd4e6185b91dd1865cf5afb669f957c6c99b2fa4
kraedal <jacobong25@icloud.com>
2024-05-22 23:22:33 +0800

    First Commit

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+import { Image } from "@nextui-org/image";
+
 import { siteConfig } from "@/config/site";
 export const Projects = () => {
   return (
-    <section className="text-gray-400 bg-gray-900 body-font" id="projects">
+    <section id="projects">
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
@@ -15,11 +17,11 @@ export const Projects = () => {
             fuga dolore.
           </p>
         </div>
-        <div className="flex flex-wrap -m-4">
+        <div className="flex flex-row">
           {siteConfig.projects.map((project) => (
             <a
               key={project.image}
-              className="sm:w-1/2 w-100 p-4"
+              className="sm:w-100 h-100 w-150 p-4 "
               href={project.link}
             >
               <div className="flex relative">
@@ -28,14 +30,14 @@ export const Projects = () => {
                   className="absolute inset-0 w-full h-full object-cover object-center"
                   src={project.image}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-90">
+                  <h2 className="tracking-widest text-sm title-font font-medium text-pink-400 mb-1">
                     {project.subtitle}
                   </h2>
                   <h1 className="title-font text-lg font-medium text-white mb-3">
                     {project.title}
                   </h1>
-                  <p className="leading-relaxed">{project.subtitle}</p>
+                  <p className="leading-relaxed">{project.description}</p>
                 </div>
               </div>
             </a>
--- config/site.ts
+++ config/site.ts
@@ -34,14 +34,27 @@ export const siteConfig = {
   projects: [
     {
       title: "My Portfolio",
-      subtitle: "My first personal portfolio.",
-      image: "../assets/portfolio.png",
+      subtitle: "My first portfolio.",
+      image:
+        "https://i.pinimg.com/originals/49/26/d0/4926d0e31b0a1cb36ab126a2f28b2185.jpg",
       link: "https://github.com/Kraedal/First-Portfolio",
+      description: "Using HTML, CSS, and JavaScript.",
     },
     {
-      title: "Voting Website Project",
+      title: "Voting Website",
       subtitle: "A voting website project.",
-      image: "/assets/voting.gif",
-    }
+      image:
+        "https://i.pinimg.com/originals/d7/16/24/d7162489c7928ad6a2a31dbf852a8d77.jpg",
+      link: " ",
+      description: "Using HTML, CSS, JS, PHP, and SQL",
+    },
+    {
+      title: "UI/UX Design",
+      subtitle: "Using Figma",
+      image:
+        "https://i.pinimg.com/originals/b4/f6/80/b4f68068600befc33101be249d9217b1.jpg",
+      link: " https://www.figma.com/design/NUYnLEjRA6Qhqz7gmyyvcI/PanTreat?node-id=14%3A224&t=Z6c2k3RjRCLYzvIU-1",
+      description: "A design for our startup.",
+    },
   ],
 };
